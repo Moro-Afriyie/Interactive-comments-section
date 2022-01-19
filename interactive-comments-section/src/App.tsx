ba3ccb4685e2d8a54bc78a6b73661c1057e74d2a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-19 10:19:15 -0100

    add custom colors to the config file

--- interactive-comments-section/src/App.tsx
+++ interactive-comments-section/src/App.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import Comment from "./components/Comment";
 import Modal from "./components/Modal";
 import SendForm from "./components/SendForm";
-// import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <Comment />
+    <div className="w-full h-full bg-moderateBlue">
+      {/* <Comment />
       <Modal />
-      <SendForm />
+      <SendForm /> */}
     </div>
   );
 }
--- interactive-comments-section/tailwind.config.js
+++ interactive-comments-section/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        moderateBlue: "hsl(238, 40%, 52%)",
+        softRed: "hsl(358, 79%, 66%)",
+        lightGrayish: "hsl(239, 57%, 85%)",
+        paleRed: "hsl(357, 100%, 86%)",
+        darkBlue: "hsl(212, 24%, 26%)",
+        grayishBlue: "hsl(211, 10%, 45%)",
+        veryLightGray: "hsl(228, 33%, 97%)",
+        lightGray: "hsl(223, 19%, 93%)",
+      },
+    },
   },
   plugins: [],
 };
