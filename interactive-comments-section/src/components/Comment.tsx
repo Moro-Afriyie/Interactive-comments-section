085fb43c66e850985c97bc0baf9726d8a759aaa3
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-19 13:24:19 -0100

    finish the card

--- interactive-comments-section/src/components/Comment.tsx
+++ interactive-comments-section/src/components/Comment.tsx
@@ -9,23 +9,36 @@ interface ICommentProps {}
 
 const Comment: React.FunctionComponent<ICommentProps> = (props) => {
   return (
-    <div className="bg-white mx-3 shadow-sm p-3 grid gap-4 rounded-md">
-      <div className="flex items-center gap-3">
-        <div className="w-10 h-10 rounded-full">
-          <img src={amyRobson} alt="profile picture w-full h-full" />
+    <div className="bg-white mx-3 shadow-sm p-3 grid gap-4 rounded-md sm:flex">
+      {/*Vote button on desktop**/}
+      <div className="sm:flex hidden py-2 px-2 max-h-20 flex-row items-center gap-3 bg-veryLightGray rounded-md w-fit sm:flex-col">
+        <img src={plus} alt="plus icon" className="cursor-pointer" />
+        <p className="text-moderateBlue text-sm font-bold">12</p>
+        <img src={minus} alt="minus icon" className="cursor-pointer" />
+      </div>
+      {/*end vote button on desktop**/}
+      <div className="info sm:flex flex-col gap-3">
+        <div className="flex items-center gap-3">
+          <div className="w-10 h-10 rounded-full">
+            <img src={amyRobson} alt="profile picture w-full h-full" />
+          </div>
+          <p className="text-darkBlue font-bold">armyrobson</p>
+          <p className="text-grayishBlue">1 month ago</p>
+          <div className="sm:flex sm:items-center hidden px-2 gap-1 cursor-pointer h-8 sm:ml-auto">
+            <img src={reply} alt="reply sign" className="w-3 h-3" />
+            <p className="text-moderateBlue font-bold">Reply</p>
+          </div>
         </div>
-        <p className="text-darkBlue font-bold">armyrobson</p>
-        <p className="text-grayishBlue">1 month ago</p>
+        <div className="sm:mb-2 sm:mr-2">
+          <p className="text-grayishBlue">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            aliquam repellendus id quaerat dolorem voluptatum et corrupti eius!
+            Ipsam quod exercitationem sunt eligendi at veritatis perferendis
+            commodi est minima quaerat!
+          </p>
+        </div>
       </div>
-      <div className="">
-        <p className="text-grayishBlue">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          aliquam repellendus id quaerat dolorem voluptatum et corrupti eius!
-          Ipsam quod exercitationem sunt eligendi at veritatis perferendis
-          commodi est minima quaerat!
-        </p>
-      </div>
-      <div className="flex items-center">
+      <div className="flex items-center sm:hidden">
         <div className="py-1 px-2 flex  flex-row items-center gap-3 bg-veryLightGray rounded-md w-fit sm:flex-col">
           <img src={plus} alt="plus icon" className="cursor-pointer" />
           <p className="text-moderateBlue text-sm font-bold">12</p>
