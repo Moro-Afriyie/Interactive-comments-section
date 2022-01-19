4ec0bc9b808a0b446b30f1b69fe50d970ad9cf5c
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-19 11:50:50 -0100

    create the card for a comment

--- interactive-comments-section/src/components/CommentCard.tsx
+++ interactive-comments-section/src/components/CommentCard.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import amyRobson from "../assets/avatars/image-amyrobson.png";
+import plus from "../assets/icon-plus.svg";
+import minus from "../assets/icon-minus.svg";
+import reply from "../assets/icon-reply.svg";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ICommentCardProps {}
@@ -14,9 +17,22 @@ const CommentCard: React.FunctionComponent<ICommentCardProps> = (props) => {
         <p className="text-darkBlue font-bold">armyrobson</p>
         <p className="text-grayishBlue">1 month ago</p>
       </div>
-      <div className="card__body">body</div>
-      <div className="card__votes">votes</div>
-      <div className="card__reply">reply</div>
+      <div className="">
+        <p className="text-grayishBlue">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          aliquam repellendus id quaerat dolorem voluptatum et corrupti eius!
+          Ipsam quod exercitationem sunt eligendi at veritatis perferendis
+          commodi est minima quaerat!
+        </p>
+      </div>
+      <div className="py-1 px-2 flex items-center gap-3 bg-veryLightGray rounded-md w-fit">
+        <img src={plus} alt="plus icon" />
+        <p className="text-moderateBlue text-sm">12</p>
+        <img src={minus} alt="minus icon" />
+      </div>
+      <div className="">
+        <img src={reply} alt="reply sign" />
+      </div>
     </div>
   );
 };
