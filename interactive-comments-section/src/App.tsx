1084154bf89ab2140ea5552249cd62fbfb200813
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-20 14:00:19 -0100

    made the subcomments responsive

--- interactive-comments-section/src/App.tsx
+++ interactive-comments-section/src/App.tsx
@@ -5,19 +5,19 @@ import SendForm from "./components/SendForm";
 function App() {
   return (
     <div className="min-h-screen bg-veryLightGray flex flex-col items-center gap-3 py-5">
-      <div className="comment max-w-3xl">
+      <div className="max-w-3xl">
         <CommentCard />
-        <div className="sub-comments py-3">
-          <div className="cards-container ml-4 sm:ml-9 pl-4 border-l border-lightGrayish flex flex-col gap-3">
+        <div className="py-3">
+          <div className="cards-container ml-4 pl-1 sm:ml-9 sm:pl-4 border-l border-lightGrayish flex flex-col gap-3">
             <CommentCard />
             <CommentCard />
           </div>
         </div>
       </div>
-      <div className="comment max-w-3xl">
+      <div className="max-w-3xl">
         <CommentCard />
-        <div className="sub-comments py-3">
-          <div className="cards-container ml-9 pl-4 border-l border-lightGrayish flex flex-col gap-3">
+        <div className="py-3">
+          <div className="cards-container ml-4 pl-1 sm:ml-9 sm:pl-4 border-l border-lightGrayish flex flex-col gap-3">
             <CommentCard />
             <CommentCard />
           </div>
