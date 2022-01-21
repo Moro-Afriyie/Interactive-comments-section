6950a1775de570c88024f0f407dc13fe210a934b
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-21 16:00:54 -0100

    finish the modal component

--- interactive-comments-section/src/components/Modal.tsx
+++ interactive-comments-section/src/components/Modal.tsx
@@ -7,16 +7,16 @@ const Modal: React.FunctionComponent<IModalProps> = (props) => {
   return (
     <div className="fixed w-full h-full top-0 left-0  inset-0 flex items-center justify-center bg-darkBlue bg-opacity-30">
       <div className="max-w-xs w-full mx-3 sm:mx-auto  p-6 bg-white flex flex-col gap-3 rounded-md">
-        <p className="text-2xl">Delete comment</p>
-        <p className="mb-4 text-sm">
+        <p className="font-medium text-lg text-darkBlue">Delete comment</p>
+        <p className="mb-4 text-sm text-grayishBlue">
           Are you sure you want to remove this comment? This will remove the
           comment and can&apos;t be undone.
         </p>
         <div className="flex gap-2">
-          <button className=" flex-1 px-4 py-2 text-white text-sm bg-darkBlue rounded-md uppercase">
+          <button className=" flex-1 px-2 sm:px-4 py-2 text-white text-sm bg-darkBlue rounded-md uppercase">
             no, cancel
           </button>
-          <button className=" flex-1 px-4 py-2 text-white text-sm bg-softRed rounded-md uppercase">
+          <button className=" flex-1 px-2 sm:px-4 py-2 text-sm text-white  bg-softRed rounded-md uppercase">
             yes, Delete
           </button>
         </div>
