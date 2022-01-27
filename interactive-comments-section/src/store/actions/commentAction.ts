bc9d90203d6bdeacf167f7a72ee96b5329efc669
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-27 12:37:17 -0100

    create the types and actions

--- interactive-comments-section/src/store/actions/commentAction.ts
+++ interactive-comments-section/src/store/actions/commentAction.ts
@@ -1,8 +1,24 @@
+import { Comment, replyCommentInterface } from "./../../interfaces/interface";
+import { ADD_NEW_COMMENT } from "./../actionTypes/commentType";
 import { GET_ALL_COMMENTS } from "../actionTypes/commentType";
 
 export const getAllComments = (data: Comment[]) => {
   return {
     type: GET_ALL_COMMENTS,
+    payload: data,
+  };
+};
+
+export const addNewComment = (data: Comment | replyCommentInterface) => {
+  return {
+    type: ADD_NEW_COMMENT,
+    payload: data,
+  };
+};
+
+export const updateComment = (data: Comment | replyCommentInterface) => {
+  return {
+    type: ADD_NEW_COMMENT,
     payload: data,
   };
 };
